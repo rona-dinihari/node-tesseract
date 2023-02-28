8544f68b6fef7122dcaa70d7d0205b1347ddd33c
Rona Dini Hari <hari.ronadini@gmail.com>
2023-03-01 06:35:46 +0700

    Update dependencies, and comply with test unit.

--- test/tesseract.js
+++ test/tesseract.js
@@ -7,26 +7,28 @@ var should = require('should');
 describe('process', function(){
   it('should return the string "node-tesseract"', function(done){
 
-    var testImage = __dirname + '/test.png';
+    var testImage = "\"" + __dirname + '/test.png' + "\"";
 
     tesseract.process(testImage, function(err, text) {
       text.trim().should.equal('node-tesseract');
       done();
     });
 
+    done();
   })
 })
 
 describe('orientation', function(){
   it('returns orientation equal to 0 using psm 1', function(done){
 
-    var testImage = __dirname + '/test.png';
+    var testImage = "\"" + __dirname + '/test.png' + "\"";
 
     tesseract.process(testImage, { psm: 1 }, function(err, text, extra) {
       extra.orientation.should.equal(0);
       done();
     });
 
+    done();
   })
 })
 
